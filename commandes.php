6ec5bcb3c664e0748c08a5606e5ced63369b8878
Nouvy <95512507+Nouvy@users.noreply.github.com>
2024-10-31 15:46:04 +0100

    First Commit

--- commandes.php
+++ commandes.php
@@ -42,7 +42,11 @@
  * )
  */
 
+
+
+"composer require darkaonline/l5-swagger"
+
+'php artisan vendor:publish --provider "L5Swagger\L5SwaggerServiceProvider"'
+
 "php artisan l5-swagger:generate"
-
-
 >
